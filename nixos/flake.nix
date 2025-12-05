1f80bef97cc727c251e71605e67bc52c2a49da21
Katsuma Ito <katsumai@gmail.com>
2025-12-06 00:44:16 +0900

    update

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+
+    nixos-hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,7 +20,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixos-hardware ... }@inputs:
   let
     system = "x86_64-linux";
     overlay = import ./pkgs;
@@ -26,6 +31,16 @@
       modules = [
         { nixpkgs.overlays = [ overlay ]; }
         ./hosts/vm/configuration.nix
+      ];
+    };
+
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = { inherit inputs; };
+      modules = [
+        { nixpkgs.overlays = [ overlay ]; }
+        ./hosts/laptop/configuration.nix
+        nixos-hardware.nixosModules.lenovo-thinkpad-x1-13th-gen
       ];
     };
 
