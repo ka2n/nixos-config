38c20ad06faa8a5de6d232dd98edbe7d89305327
Katsuma Ito <katsumai@gmail.com>
2026-01-10 13:39:06 +0900

    devShells: volta

--- devShells.nix
+++ devShells.nix
@@ -15,8 +15,18 @@ in {
     ];
 
     shellHook = ''
+      # Playwright configuration
       export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
       export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+
+      # Volta configuration
+      export VOLTA_HOME="$HOME/.volta"
+      export PATH="$VOLTA_HOME/bin:$PATH"
+      export VOLTA_FEATURE_PNPM=1
+
+      # NixOS compatibility for Volta-installed binaries
+      export NIX_LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (with pkgs; [ stdenv.cc.cc openssl ])}"
+      export NIX_LD="${pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"}"
     '';
   };
 }
