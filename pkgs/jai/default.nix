42c32745e2965845610b27f9a1c683e40a42998c
Katsuma Ito <katsumai@gmail.com>
2026-04-08 12:32:13 +0900

    chore: update jai config

--- pkgs/jai/default.nix
+++ pkgs/jai/default.nix
@@ -1,39 +1,25 @@
-{
-  lib,
-  gcc15Stdenv,
-  fetchFromGitHub,
-  autoreconfHook,
-  pkg-config,
-  pandoc,
-  util-linux,
-  acl,
-}:
+{ lib, gcc15Stdenv, fetchFromGitHub, autoreconfHook, pkg-config, pandoc
+, util-linux, acl, }:
 
 gcc15Stdenv.mkDerivation {
   pname = "jai";
-  version = "0.2-unstable-2026-03-27";
+  version = "0.3";
 
   src = fetchFromGitHub {
     owner = "stanford-scs";
     repo = "jai";
-    rev = "46f507a4e0b4eb87950a095d19bbf837d79ecf57";
-    hash = "sha256-OZrsNJrKe3gH3sGefrKgnFmDiMMP+xr4eM90hdGEDdg=";
+    rev = "7552ddea1633710538317a995d98421276fb0eca";
+    hash = "sha256-AByC7Xh1FYbQ/4Au396m2zYUxsLqcF1PEbpdz7x6LaQ=";
   };
 
-  nativeBuildInputs = [
-    autoreconfHook
-    pkg-config
-    pandoc
-  ];
+  nativeBuildInputs = [ autoreconfHook pkg-config pandoc ];
 
   buildInputs = [
     util-linux # libmount
     acl # libacl
   ];
 
-  configureFlags = [
-    "--with-untrusted-user=nobody"
-  ];
+  configureFlags = [ "--with-untrusted-user=nobody" ];
 
   # Skip setuid install hooks (nix store is read-only)
   postInstall = ''
